0171dfc66fa75bff225b2ed082379fe3d321e243
ArkadySkrynnikov <catmaster1488@gmail.com>
2024-03-27 12:09:12 +0700

    fix(ui): storybook

--- src/shared/UI/button/Button.stories.tsx
+++ src/shared/UI/button/Button.stories.tsx
@@ -1,4 +1,4 @@
-import Button from '../button/Button.tsx'
+import { Button } from '../button/Button.tsx'
 import type { Meta, StoryObj } from '@storybook/react'
 
 const meta: Meta<typeof Button> = {
--- src/shared/UI/link/Link.stories.ts
+++ src/shared/UI/link/Link.stories.ts
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react'
-import Link from './Link.tsx'
+import { Link } from './Link.tsx'
 import {
     reactRouterParameters,
     withRouter,
