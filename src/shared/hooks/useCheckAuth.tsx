86cae84312f1a9b32df6255815b73ede193357d2
dshuv <shuvalovdev@gmail.com>
2024-03-27 15:32:34 +0300

    fix(auth ): fixed useAuthHook

--- src/shared/hooks/useCheckAuth.tsx
+++ src/shared/hooks/useCheckAuth.tsx
@@ -1,5 +1,5 @@
 import { getAuth, onAuthStateChanged, User } from 'firebase/auth'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 interface IFirebaseUser {
     email: string
@@ -11,15 +11,20 @@ interface IFirebaseUser {
 export const useCheckAuth = () => {
     const auth = getAuth()
     const [user, setUser] = useState<IFirebaseUser | null>(null)
-    onAuthStateChanged(auth, (res: User | null) => {
-        if (res) {
-            setUser({
-                email: res.email ?? '',
-                displayName: res.displayName ?? '',
-                uid: res.uid,
-                refreshToken: res.refreshToken,
-            })
-        }
-    })
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (res: User | null) => {
+            if (res) {
+                setUser({
+                    email: res.email ?? '',
+                    displayName: res.displayName ?? '',
+                    uid: res.uid,
+                    refreshToken: res.refreshToken,
+                })
+            }
+        })
+        return () => unsubscribe()
+    }, [auth])
+
     return user
 }
