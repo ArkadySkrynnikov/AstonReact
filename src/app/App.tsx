0e8d0e939e5f1e267692e4a03cef56231ed79d08
ArkadySkrynnikov <catmaster1488@gmail.com>
2024-04-05 20:10:48 +0700

    fix(app): theme)

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,11 +2,17 @@ import { RouterProvider } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { store } from './providers/store/store.tsx'
 import { router } from './providers/router/router.tsx'
+import { ErrorBoundary } from 'react-error-boundary'
+import { ThemeProvider } from './context/ThemeContext.tsx'
 
 export const App = () => {
     return (
-        <Provider store={store}>
-            <RouterProvider router={router} />
-        </Provider>
+        <ErrorBoundary fallback={<div>Loading...</div>}>
+            <ThemeProvider>
+                <Provider store={store}>
+                    <RouterProvider router={router} />
+                </Provider>
+            </ThemeProvider>
+        </ErrorBoundary>
     )
 }
